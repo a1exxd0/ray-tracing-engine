155f157fbfd3b5c4a4e0a88dcdb9e67f53027cff
Alex Do <alexxdo2004@gmail.com>
2024-06-16 16:49:15 +0100

    test: added more tests for color class and renamed tests for readability

--- tests/color_class_tests.cpp
+++ tests/color_class_tests.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 #include "color3.h"
 
-TEST(Example_Test, Add_Colors) {
+TEST(Color_Class_Test, Add_Colors) {
     // Arrange
     rte::color3 expected(20.0, 50.0, 100.0);
     // Act
@@ -14,7 +14,7 @@ TEST(Example_Test, Add_Colors) {
     EXPECT_EQ(result, expected);
 }
 
-TEST(Example_Test, Multiply_Colors) {
+TEST(Color_Class_Test, Multiply_Colors) {
     // Arrange
     rte::color3 expected(20.0, 50.0, 100.0);
 
@@ -27,7 +27,7 @@ TEST(Example_Test, Multiply_Colors) {
     EXPECT_EQ(result, expected);
 }
 
-TEST(Example_Test, Loop_Many_Additions) {
+TEST(Color_Class_Test, Loop_Many_Additions) {
     // Arrange
     rte::color3 expected(1000000.0, 1000000.0, 1000000.0);
 
@@ -39,6 +39,54 @@ TEST(Example_Test, Loop_Many_Additions) {
         result += temp;
     }
 
+    EXPECT_EQ(result, expected);
+}
+
+TEST(Color_Class_Test, Scalar_Multiply_Colors) {
+    // Arrange
+    rte::color3 expected(20.0, 50.0, 100.0);
+
+    // Act
+    rte::color3 prev(10.0 , 25.0 , 50.0);
+    rte::color3 result = 2 * prev;
+
+    // Assert
+    EXPECT_EQ(result, expected);
+}
+
+TEST(Color_Class_Test, Scalar_Multiply_Colors_Reverse) {
+    // Arrange
+    rte::color3 expected(20.0, 50.0, 100.0);
+
+    // Act
+    rte::color3 prev(10.0 , 25.0 , 50.0);
+    rte::color3 result = prev * 2;
+
+    // Assert
+    EXPECT_EQ(result, expected);
+}
+
+TEST(Color_Class_Test, Scalar_Divide_Colors) {
+    // Arrange
+    rte::color3 expected(5.0, 12.5, 25.0);
+
+    // Act
+    rte::color3 prev(10.0 , 25.0 , 50.0);
+    rte::color3 result = prev / 2;
+
+    // Assert
+    EXPECT_EQ(result, expected);
+}
+
+TEST(Color_Class_Test, Unit_Vector) {
+    // Arrange
+    rte::color3 expected(1 / sqrt(3), 1 / sqrt(3), 1 / sqrt(3));
+
+    // Act
+    rte::color3 prev(1.0, 1.0, 1.0);
+    rte::color3 result = unit_vector(prev);
+
+    // Assert
     EXPECT_EQ(result, expected);
 }
 
