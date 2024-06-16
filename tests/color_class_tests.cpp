d9a4483e1ff3da7fc38e45c9ba3692f987743342
Alex Do <alexxdo2004@gmail.com>
2024-06-16 15:22:21 +0100

    test: added longer test for looped addition

--- tests/color_class_tests.cpp
+++ tests/color_class_tests.cpp
@@ -17,13 +17,28 @@ TEST(Example_Test, Add_Colors) {
 TEST(Example_Test, Multiply_Colors) {
     // Arrange
     rte::color3 expected(20.0, 50.0, 100.0);
+
     // Act
     rte::color3 prev(10.0 , 25.0 , 50.0);
     rte::color3 multiply(2.0, 2.0, 2.0);
     rte::color3 result = prev * multiply;
 
-
     // Assert
+    EXPECT_EQ(result, expected);
+}
+
+TEST(Example_Test, Loop_Many_Additions) {
+    // Arrange
+    rte::color3 expected(1000000.0, 1000000.0, 1000000.0);
+
+    // Act
+    rte::color3 temp(1.0, 1.0, 1.0);
+    rte::color3 result;
+
+    for (int i = 0; i < 1000000; i++) {
+        result += temp;
+    }
+
     EXPECT_EQ(result, expected);
 }
 
