b2f53f300da07ebe8f8469e9c2f3a3c150408024
Alex Do <alexxdo2004@gmail.com>
2024-06-18 14:24:56 +0100

    test: add more benchmark features

--- rte_benchmark/rte_benchmark_main.cpp
+++ rte_benchmark/rte_benchmark_main.cpp
@@ -10,11 +10,24 @@ static void BM_Add_Colors(benchmark::State& state) {
             result += temp;
         }
     }
-    
+}
+
+static void BM_Multiply_Colors(benchmark::State& state) {
+    for (auto _ : state) {
+        rte::color3 temp(0.9, 0.9, 0.9);
+        for (int i = 0; i < 100000; i++) {
+            temp *= 0.9;
+        }
+    }
 }
 
 BENCHMARK(BM_Add_Colors)
 ->Name("Add multiple colors together")
+->Iterations(1000)
+->MeasureProcessCPUTime();
+
+BENCHMARK(BM_Multiply_Colors)
+->Name("Scalar multiply a colour many times")
 ->Iterations(1000)
 ->MeasureProcessCPUTime();
 
